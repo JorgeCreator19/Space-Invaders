d4f8f1f130f3d9cf6157470b556e030879e169fc
JorgeCreator19 <jorgitoferrando10@gmail.com>
2026-01-15 20:47:31 +0100

    Implement main method to launch the Space Invaders game

--- src/com/spaceinvaders/Main.java
+++ src/com/spaceinvaders/Main.java
@@ -1,5 +1,19 @@
 package com.spaceinvaders;
 
+import com.spaceinvaders.game.GameFrame;
+import javax.swing.SwingUtilities;
+
+/**
+ * The main to run the space invaders game
+ */
 public class Main {
-    
+    public static void main(String[] args) {
+        // Run on Swing's Event Dispatch Thread (this required for GUI)
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new GameFrame();
+            }
+        });
+    }
 }
